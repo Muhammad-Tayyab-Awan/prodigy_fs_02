9a026fa7c635097eccfa5d5ed940e1849ea0e597
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-16 07:38:50 +0500

    modified auth route uri

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ import authRoute from "./routes/auth.js";
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use("auth", authRoute);
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   try {
