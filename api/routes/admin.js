6cda45e16e0a785b7cbc97ac655b381a972690e7
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-17 16:01:40 +0500

    added endpoint to mark specific user's attendance

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -336,4 +336,71 @@ router.get(
   },
 );
 
+router.post(
+  "/attendance/:userId",
+  [
+    param("userId").isMongoId(),
+    body("status").isIn(
+      ["absent", "present", "leave"],
+      body("markedOn").isISO8601({ strictSeparator: true }),
+    ),
+  ],
+  async (req, res) => {
+    try {
+      const { userStatus } = req;
+      if (!(userStatus.loggedIn && userStatus.role === "admin"))
+        return res.status(404).json({
+          resStatus: false,
+          error: "Please login to your admin account",
+        });
+      const result = validationResult(req);
+      if (!result.isEmpty())
+        return res.status(400).json({ resStatus: false, error: result.errors });
+      const { userId } = req.params;
+      const userExist = await User.findById(userId);
+      if (!(userExist && userExist.role === "user"))
+        return res
+          .status(404)
+          .json({ resStatus: false, error: "No user exist with that id" });
+      if (!userExist.userVerified)
+        return res.status(404).json({
+          resStatus: false,
+          error: "User you are looking for is not yet verified",
+        });
+      const { status, markedOn } = req.body;
+      const attendanceMarked = await Attendance.findOne({
+        employeeId: userId,
+        markedOn: new Date(markedOn).toLocaleString().split(",")[0],
+      });
+      if (attendanceMarked)
+        return res.status(400).json({
+          resStatus: false,
+          error: `User's attendance already marked for ${markedOn}`,
+        });
+      const newAttendance = await Attendance.create({
+        employeeId: userId,
+        markedOn: new Date(markedOn).toLocaleString().split(",")[0],
+        status: status,
+      });
+      if (status === "leave") {
+        await Leave.create({
+          employeeId: userId,
+          attendanceId: newAttendance.id,
+          status: "approved",
+        });
+      }
+      res.status(200).json({
+        resStatus: true,
+        message: "User attendance is successfully marked",
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: "Error Occurred on Server Side",
+        message: error.message,
+      });
+    }
+  },
+);
+
 export default router;
