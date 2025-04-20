0db65c7182a73becdaf4dcd7f6562cf24fc286a2
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-20 13:03:41 +0500

    added useAuth hook to manage user authentication status

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import authApi from "../api/authApi";
+
+const useAuth = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState({ loggedIn: null, role: null });
+  useEffect(() => {
+    authApi.getLoginStatus().then((response) => {
+      setIsLoggedIn(response);
+    });
+  }, []);
+  if (isLoggedIn.loggedIn === null) {
+    return { isLoading: true };
+  }
+  if (!isLoggedIn) {
+    return { isLoggedIn: false, redirectToLogin: true };
+  }
+  return { isLoggedIn: true, role: isLoggedIn.role };
+};
+
+export default useAuth;
