714306caa2ffb6cb6e2abe24ca297978f3200222
Muhammad-Tayyab-Awan <mtayyabawan1918@gmail.com>
2025-04-25 15:18:07 +0500

    added not-found page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router";
+
 function NotFound() {
-  return <div>not-found</div>;
+  return (
+    <div className="min-h-screen w-full overflow-auto bg-purple-400">
+      <h1 className="mt-20 text-center font-serif text-6xl font-medium text-red-600 text-shadow-black text-shadow-lg">
+        404
+      </h1>
+      <p className="mt-5 w-full text-center text-lg font-medium text-black">
+        Page not found
+      </p>
+      <Link
+        to="/"
+        className="mx-auto mt-4 block w-fit rounded-sm bg-black px-2 py-1 text-sm font-medium text-white"
+      >
+        Back to home
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
